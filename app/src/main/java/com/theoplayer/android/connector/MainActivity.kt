8c3b3657e700148148b5b4f5ff06ec0b1cf258d6
Hovig <hovig.jilinkerian@theoplayer.com>
2022-09-06 13:22:02 +0200

    initial commit

---
+++ app/src/main/java/com/theoplayer/android/connector/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.theoplayer.android.connector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
