4ee11eead79cde197dc8017f2cb2617b1acd4b1d
Hovig <hovig.jilinkerian@theoplayer.com>
2022-09-14 15:55:39 +0200

    use conviva connector in app

--- app/src/main/java/com/theoplayer/android/connector/MainActivity.kt
+++ app/src/main/java/com/theoplayer/android/connector/MainActivity.kt
@@ -6,20 +6,28 @@ import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.conviva.api.SystemSettings
+import com.conviva.sdk.ConvivaSdkConstants
 import com.theoplayer.android.api.THEOplayerConfig
 import com.theoplayer.android.api.THEOplayerView
+import com.theoplayer.android.api.ads.ima.GoogleImaIntegrationFactory
 import com.theoplayer.android.api.source.SourceDescription
 import com.theoplayer.android.api.source.TypedSource
+import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription
+import com.theoplayer.android.connector.analytics.conviva.ConvivaConnector
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var theoplayerView: THEOplayerView
+    private var convivaConnector: ConvivaConnector? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupTHEOplayer()
+        setupGoogleImaIntegration()
+        setupConviva()
     }
 
     private fun setupTHEOplayer() {
@@ -31,12 +39,35 @@ class MainActivity : AppCompatActivity() {
         tpvContainer.addView(theoplayerView)
     }
 
+    private fun setupGoogleImaIntegration() {
+        val googleImaIntegration = GoogleImaIntegrationFactory.createGoogleImaIntegration(theoplayerView)
+        theoplayerView.player.addIntegration(googleImaIntegration)
+    }
+
+    private fun setupConviva() {
+        val customerKey = "your_conviva_customer_key"
+        val gatewayUrl = "your_conviva_debug_gateway_url"
+
+        val settings = HashMap<String, Any>()
+        settings[ConvivaSdkConstants.GATEWAY_URL] = gatewayUrl
+        settings[ConvivaSdkConstants.LOG_LEVEL] = SystemSettings.LogLevel.DEBUG
+
+        convivaConnector = ConvivaConnector(applicationContext, theoplayerView.player, customerKey, settings)
+        convivaConnector?.setViewerId("viewer ID")
+    }
+
     fun setSource(view: View) {
         theoplayerView.player.source = SourceDescription.Builder(
             TypedSource.Builder("https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8")
                 .build()
         )
+        .ads(
+            GoogleImaAdDescription.Builder("https://cdn.theoplayer.com/demos/ads/vast/dfp-linear-inline-no-skip.xml")
+                .timeOffset("5")
+                .build()
+        )
         .build()
+        convivaConnector?.setAssetName("BigBuckBunny with Google IMA ads")
     }
 
     fun playPause(view: View) {
