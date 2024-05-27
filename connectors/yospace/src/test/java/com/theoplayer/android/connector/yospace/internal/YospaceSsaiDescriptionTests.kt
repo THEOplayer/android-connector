b29ec0911dbefdc8bd4c0b3d76ebacec52d3cf74
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-05-27 18:46:44 +0200

    Test serialization

---
+++ connectors/yospace/src/test/java/com/theoplayer/android/connector/yospace/internal/YospaceSsaiDescriptionTests.kt
@@ -0,0 +1,76 @@
+package com.theoplayer.android.connector.yospace.internal
+
+import com.theoplayer.android.connector.yospace.INTEGRATION_ID
+import com.theoplayer.android.connector.yospace.YospaceSsaiDescription
+import com.theoplayer.android.connector.yospace.YospaceSsaiDescriptionSerializer
+import com.theoplayer.android.connector.yospace.YospaceStreamType
+import com.yospace.admanagement.Session.SessionProperties
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonObject
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class YospaceSsaiDescriptionSerializerTests {
+    private lateinit var serializer: YospaceSsaiDescriptionSerializer
+
+    @Before
+    fun setup() {
+        serializer = YospaceSsaiDescriptionSerializer()
+    }
+
+    @Test
+    fun givenEmptySsaiDescription_WhenSerialize_ThenReturnsExpected() {
+        val ssaiDescription = YospaceSsaiDescription()
+        val jsonString = serializer.toJson(ssaiDescription)
+        val jsonObject = Json.parseToJsonElement(jsonString).jsonObject
+        assertEquals(jsonObject["integration"], JsonPrimitive(INTEGRATION_ID))
+        assertEquals(jsonObject["streamType"], JsonPrimitive(YospaceStreamType.LIVE.toString()))
+        val jsonSessionProperties = jsonObject["sessionProperties"]?.jsonObject
+        assertEquals(
+            jsonSessionProperties?.keys, setOf(
+                "requestTimeout",
+                "resourceTimeout",
+                "userAgent",
+                "proxyUserAgent",
+                "keepProxyAlive",
+                "prefetchResources",
+                "fireHistoricalBeacons",
+                "applyEncryptedTracking",
+                "excludedCategories",
+                "consecutiveBreakTolerance",
+                "token",
+                "customHttpHeaders"
+            )
+        )
+    }
+
+    @Test
+    fun givenSsaiDescriptionWithStreamType_WhenSerialize_ThenReturnsExpected() {
+        val ssaiDescription = YospaceSsaiDescription(
+            streamType = YospaceStreamType.LIVEPAUSE
+        )
+        val jsonString = serializer.toJson(ssaiDescription)
+        val jsonObject = Json.parseToJsonElement(jsonString).jsonObject
+        assertEquals(jsonObject["streamType"], JsonPrimitive(YospaceStreamType.LIVEPAUSE.toString()))
+    }
+
+    @Test
+    fun givenSsaiDescriptionWithSessionProperties_WhenSerialize_ThenReturnsExpected() {
+        val testUserAgent = "Test User Agent"
+        val testHeaders = mapOf("X-Hello" to "World")
+        val ssaiDescription = YospaceSsaiDescription(
+            sessionProperties = SessionProperties().apply {
+                userAgent = testUserAgent
+                customHttpHeaders = testHeaders
+            }
+        )
+        val jsonString = serializer.toJson(ssaiDescription)
+        val jsonObject = Json.parseToJsonElement(jsonString).jsonObject
+        val jsonSessionProperties = jsonObject["sessionProperties"]?.jsonObject
+        assertEquals(jsonSessionProperties?.get("userAgent"), JsonPrimitive(testUserAgent))
+        assertEquals(jsonSessionProperties?.get("customHttpHeaders"), JsonObject(testHeaders.mapValues { JsonPrimitive(it.value) }))
+    }
+}
