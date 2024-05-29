0f1311ea56c707824e8eaefb9ab0850196a1735a
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-05-29 11:48:43 +0200

    Add Yospace test sources to sample app

--- app/src/main/java/com/theoplayer/android/connector/Sources.kt
+++ app/src/main/java/com/theoplayer/android/connector/Sources.kt
@@ -1,9 +1,12 @@
 package com.theoplayer.android.connector
 
 import com.theoplayer.android.api.source.SourceDescription
+import com.theoplayer.android.api.source.SourceType
 import com.theoplayer.android.api.source.TypedSource
 import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription
 import com.theoplayer.android.api.source.metadata.MetadataDescription
+import com.theoplayer.android.connector.yospace.YospaceSsaiDescription
+import com.theoplayer.android.connector.yospace.YospaceStreamType
 
 data class Source(
     val name: String,
@@ -31,6 +34,71 @@ val sources: List<Source> by lazy {
                 "assetid" to "C112233",
                 "program" to "BigBuckBunny with Google IMA ads"
             )
+        ),
+        Source(
+            name = "Yospace HLS VOD",
+            sourceDescription = SourceDescription
+                .Builder(
+                    TypedSource.Builder(
+                        "https://csm-e-sdk-validation.bln1.yospace.com/csm/access/156611618/c2FtcGxlL21hc3Rlci5tM3U4?yo.av=4"
+                    )
+                        .type(SourceType.HLS)
+                        .ssai(YospaceSsaiDescription(streamType = YospaceStreamType.VOD))
+                        .build()
+                )
+                .build()
+        ),
+        Source(
+            name = "Yospace HLS Live",
+            sourceDescription = SourceDescription
+                .Builder(
+                    TypedSource.Builder(
+                        "https://csm-e-sdk-validation.bln1.yospace.com/csm/extlive/yospace02,hlssample42.m3u8?yo.br=true&yo.av=4"
+                    )
+                        .type(SourceType.HLS)
+                        .ssai(YospaceSsaiDescription(streamType = YospaceStreamType.LIVE))
+                        .build()
+                )
+                .build()
+        ),
+        Source(
+            name = "Yospace HLS DVRLive",
+            sourceDescription = SourceDescription
+                .Builder(
+                    TypedSource.Builder(
+                        "https://csm-e-sdk-validation.bln1.yospace.com/csm/extlive/yospace02,hlssample42.m3u8?yo.br=true&yo.lp=true&yo.av=4"
+                    )
+                        .type(SourceType.HLS)
+                        .ssai(YospaceSsaiDescription(streamType = YospaceStreamType.LIVEPAUSE))
+                        .build()
+                )
+                .build()
+        ),
+        Source(
+            name = "Yospace DASH Live",
+            sourceDescription = SourceDescription
+                .Builder(
+                    TypedSource.Builder(
+                        "https://csm-e-sdk-validation.bln1.yospace.com/csm/extlive/yosdk01,t2-dash.mpd?yo.br=true&yo.av=4"
+                    )
+                        .type(SourceType.DASH)
+                        .ssai(YospaceSsaiDescription(streamType = YospaceStreamType.LIVE))
+                        .build()
+                )
+                .build()
+        ),
+        Source(
+            name = "Yospace DASH DVRLive",
+            sourceDescription = SourceDescription
+                .Builder(
+                    TypedSource.Builder(
+                        "https://csm-e-sdk-validation.bln1.yospace.com/csm/extlive/yosdk01,dash.mpd?yo.br=true&yo.lp=true&yo.jt=1000&yo.av=4"
+                    )
+                        .type(SourceType.DASH)
+                        .ssai(YospaceSsaiDescription(streamType = YospaceStreamType.LIVEPAUSE))
+                        .build()
+                )
+                .build()
         )
     )
 }
