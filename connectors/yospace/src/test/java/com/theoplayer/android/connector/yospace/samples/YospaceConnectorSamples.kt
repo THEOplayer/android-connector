2c12c236fd3f9b8a95adde72b0f2fc8cd9c70828
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-06-03 13:26:03 +0200

    Improve sample

--- connectors/yospace/src/test/java/com/theoplayer/android/connector/yospace/samples/YospaceConnectorSamples.kt
+++ connectors/yospace/src/test/java/com/theoplayer/android/connector/yospace/samples/YospaceConnectorSamples.kt
@@ -2,9 +2,23 @@ package com.theoplayer.android.connector.yospace.samples
 
 import android.app.Activity
 import com.theoplayer.android.api.THEOplayerView
+import com.theoplayer.android.api.source.SourceDescription
+import com.theoplayer.android.api.source.TypedSource
 import com.theoplayer.android.connector.yospace.YospaceConnector
+import com.theoplayer.android.connector.yospace.YospaceSsaiDescription
+import com.theoplayer.android.connector.yospace.YospaceStreamType
 
 fun createYospaceConnector(activity: Activity) {
+    // Set up the player and the connector
     val theoplayerView = THEOplayerView(activity)
     val yospaceConnector = YospaceConnector(theoplayerView)
+
+    // Play a stream with Yospace SSAI
+    theoplayerView.player.source = SourceDescription.Builder(
+        TypedSource.Builder("https://example.com/stream.m3u8")
+            .ssai(
+                YospaceSsaiDescription(streamType = YospaceStreamType.LIVE)
+            )
+            .build()
+    ).build()
 }
