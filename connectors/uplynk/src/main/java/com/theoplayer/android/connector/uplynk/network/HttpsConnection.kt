585d8c830ffa456a311ae82fadd140c87cdb3770
OlegRyz <aleh.ryzhankou@gmail.com>
2024-07-31 16:31:19 +0200

    Refactor HttpsConnection

--- connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/network/HttpsConnection.kt
+++ connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/network/HttpsConnection.kt
@@ -2,44 +2,42 @@ package com.theoplayer.android.connector.uplynk.network
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runInterruptible
-import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
+internal const val CONNECT_TIMEOUT_IN_MS = 30000
+internal const val READ_TIMEOUT_IN_MS = 30000
 
 internal class HttpsConnection {
     suspend fun get(urlString: String): String = runInterruptible(context = Dispatchers.IO) {
             var result: String
-            var urlConnection: HttpsURLConnection? = null
+            var connection: HttpsURLConnection? = null
             try {
-                val url = URL(urlString)
-                urlConnection = url.openConnection() as HttpsURLConnection
-                urlConnection.setRequestMethod("GET")
-                urlConnection.setReadTimeout(10000)
-                urlConnection.setConnectTimeout(15000)
-                urlConnection.setDoInput(true)
-
-                urlConnection.connect()
-
-                val responseCode: Int = urlConnection.getResponseCode()
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    val reader = BufferedReader(InputStreamReader(urlConnection.getInputStream()))
-                    var inputLine: String?
-                    val response = StringBuilder()
-
-                    while ((reader.readLine().also { inputLine = it }) != null) {
-                        response.append(inputLine)
+                connection = (URL(urlString).openConnection() as HttpsURLConnection)
+                    .apply {
+                        requestMethod = "GET"
+                        connectTimeout = CONNECT_TIMEOUT_IN_MS
+                        readTimeout = READ_TIMEOUT_IN_MS
+                        doInput = true
                     }
+
+                connection.connect()
+
+                val responseCode: Int = connection.getResponseCode()
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    val reader = InputStreamReader(connection.inputStream)
+                    result = reader
+                        .buffered()
+                        .readText()
                     reader.close()
-                    result = response.toString()
                 } else {
                     result = "Error: $responseCode"
                 }
             } catch (e: Exception) {
                 result = e.message ?: ""
             } finally {
-                urlConnection?.disconnect()
+                connection?.disconnect()
             }
 
             result
