8f50d3d8af00baea71614d4034111cac37532610
OlegRyz <aleh.ryzhankou@gmail.com>
2024-08-19 12:16:59 +0200

    Add docs for UplynkListener

--- connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/UplynkListener.kt
+++ connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/UplynkListener.kt
@@ -3,8 +3,28 @@ package com.theoplayer.android.connector.uplynk
 import com.theoplayer.android.connector.uplynk.network.AssetInfoResponse
 import com.theoplayer.android.connector.uplynk.network.PreplayResponse
 
+/**
+ * A listener interface for receiving events related to Uplynk
+ * Implementations of this interface can be used to handle responses from Uplynk's API.
+ */
 interface UplynkListener {
-    fun onPreplayResponse(response: PreplayResponse)
-    fun onAssetInfoResponse(response: AssetInfoResponse)
+
+    /**
+     * Called when a preplay response is received from Uplynk.
+     *
+     * For more details, refer to the [Preplay API (Version 2) Documentation](https://docs.edgecast.com/video/index.html#Develop/Preplayv2.htm).
+     *
+     * @param response the `PreplayResponse` object containing information relevant to the preplay request.
+     */
+    fun onPreplayResponse(response: PreplayResponse) {}
+
+    /**
+     * Called when an asset information response is received from Uplynk.
+     *
+     * For more details, refer to the [Asset Info Documentation](https://docs.edgecast.com/video/index.html#Develop/AssetInfo.htm).
+     *
+     * @param response the `AssetInfoResponse` object containing detailed information about the asset.
+     */
+    fun onAssetInfoResponse(response: AssetInfoResponse) {}
 
 }
