9898287984b23ef9f15b2deaaddf9cf6f8168e85
OlegRyz <aleh.ryzhankou@gmail.com>
2024-09-04 12:12:57 +0200

    Implement Retry logic for requests

--- connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/internal/network/HttpsConnection.kt
+++ connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/internal/network/HttpsConnection.kt
@@ -1,10 +1,13 @@
 package com.theoplayer.android.connector.uplynk.internal.network
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runInterruptible
 import java.io.IOException
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 internal const val CONNECT_TIMEOUT_IN_MS = 30000
 internal const val READ_TIMEOUT_IN_MS = 30000
@@ -35,5 +38,25 @@ internal class HttpsConnection {
         } finally {
             connection?.disconnect()
         }
+    }
+
+    suspend fun retry(
+        retry: Int = 3,
+        initialDelay: Duration = 300.milliseconds,
+        maxDelay: Duration = 5000.milliseconds,
+        delayMultiplier: Double = 2.0,
+        request: suspend HttpsConnection.() -> String
+    ): String {
+        var delayDuration = initialDelay
+        for (remains in retry - 1 downTo 0) {
+            try {
+                return request()
+            } catch (e: IOException) {
+                if (remains == 0) throw IOException(e)
+                delay(delayDuration)
+                delayDuration = (delayDuration * delayMultiplier).coerceAtMost(maxDelay)
+            }
+        }
+        throw IllegalStateException("Retry count should be bigger than 0 but it is $retry")
     }
 }
