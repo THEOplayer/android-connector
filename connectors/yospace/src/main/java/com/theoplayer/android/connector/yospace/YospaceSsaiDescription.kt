19d793bf260c1029096f6c56cf43b16ee65a8993
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-01-16 11:01:54 +0100

    Simplify serializing YospaceSsaiDescription

--- connectors/yospace/src/main/java/com/theoplayer/android/connector/yospace/YospaceSsaiDescription.kt
+++ connectors/yospace/src/main/java/com/theoplayer/android/connector/yospace/YospaceSsaiDescription.kt
@@ -2,31 +2,37 @@ package com.theoplayer.android.connector.yospace
 
 import com.theoplayer.android.api.source.ssai.CustomSsaiDescription
 import com.theoplayer.android.api.source.ssai.CustomSsaiDescriptionSerializer
-import com.theoplayer.android.connector.yospace.internal.SerializedSessionProperties
-import com.theoplayer.android.connector.yospace.internal.deserialize
-import com.theoplayer.android.connector.yospace.internal.serialize
+import com.theoplayer.android.connector.yospace.internal.YospaceSessionPropertiesKSerializer
 import com.yospace.admanagement.Session
+import kotlinx.serialization.EncodeDefault
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.JsonTransformingSerializer
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 /**
  * The configuration for server-side ad insertion using the [YospaceConnector].
  */
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
 data class YospaceSsaiDescription @JvmOverloads constructor(
     /**
      * The type of the requested stream.
      *
      * Default: [YospaceStreamType.LIVE]
      */
+    @EncodeDefault
     val streamType: YospaceStreamType = YospaceStreamType.LIVE,
     /**
      * Custom properties to set when initializing the Yospace session.
      */
+    @EncodeDefault
+    @Serializable(with = YospaceSessionPropertiesKSerializer::class)
     val sessionProperties: Session.SessionProperties = Session.SessionProperties()
 ) : CustomSsaiDescription() {
     override val customIntegration: String
@@ -83,43 +89,33 @@ enum class YospaceStreamType {
 
 internal class YospaceSsaiDescriptionSerializer : CustomSsaiDescriptionSerializer {
     override fun fromJson(json: String): YospaceSsaiDescription {
-        return Json.decodeFromString(YospaceSsaiDescriptionKSerializer(), json)
+        return Json.decodeFromString(YospaceSsaiDescriptionKSerializer, json)
     }
 
     override fun toJson(value: CustomSsaiDescription): String {
-        return Json.encodeToString(YospaceSsaiDescriptionKSerializer(), value as YospaceSsaiDescription)
+        return Json.encodeToString(
+            YospaceSsaiDescriptionKSerializer,
+            value as YospaceSsaiDescription
+        )
     }
 }
 
-@Serializable
-private data class SerializedYospaceSsaiDescription(
-    val integration: String,
-    val streamType: YospaceStreamType,
-    val sessionProperties: SerializedSessionProperties
-)
+private object YospaceSsaiDescriptionKSerializer :
+    JsonTransformingSerializer<YospaceSsaiDescription>(YospaceSsaiDescription.serializer()) {
 
-private fun YospaceSsaiDescription.serialize() = SerializedYospaceSsaiDescription(
-    integration = customIntegration,
-    streamType = streamType,
-    sessionProperties = sessionProperties.serialize()
-)
+    override fun transformSerialize(element: JsonElement): JsonElement = JsonObject(
+        // Add integration to JSON
+        buildMap {
+            put("integration", JsonPrimitive(YospaceConnector.INTEGRATION_ID))
+            putAll(element.jsonObject)
+        }
+    )
 
-private fun SerializedYospaceSsaiDescription.deserialize() = YospaceSsaiDescription(
-    streamType = streamType,
-    sessionProperties = sessionProperties.deserialize()
-)
-
-private class YospaceSsaiDescriptionKSerializer : KSerializer<YospaceSsaiDescription> {
-    private val delegateSerializer = SerializedYospaceSsaiDescription.serializer()
-
-    @OptIn(ExperimentalSerializationApi::class)
-    override val descriptor = SerialDescriptor("YospaceSsaiDescription", delegateSerializer.descriptor)
-
-    override fun serialize(encoder: Encoder, value: YospaceSsaiDescription) {
-        encoder.encodeSerializableValue(delegateSerializer, value.serialize())
+    override fun transformDeserialize(element: JsonElement): JsonElement {
+        // Validate integration in JSON
+        val integration = element.jsonObject["integration"]?.jsonPrimitive
+        require(integration == JsonPrimitive(YospaceConnector.INTEGRATION_ID))
+        return element
     }
 
-    override fun deserialize(decoder: Decoder): YospaceSsaiDescription {
-        return decoder.decodeSerializableValue(delegateSerializer).deserialize()
-    }
 }
