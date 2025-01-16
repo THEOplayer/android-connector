8b4d994c1835ce0c9b518502a9f8349c44ed30e4
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-01-16 11:38:37 +0100

    Add integration to UplynkSsaiDescription JSON

--- connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/UplynkSsaiDeserializer.kt
+++ connectors/uplynk/src/main/java/com/theoplayer/android/connector/uplynk/UplynkSsaiDeserializer.kt
@@ -2,15 +2,43 @@ package com.theoplayer.android.connector.uplynk
 
 import com.theoplayer.android.api.source.ssai.CustomSsaiDescription
 import com.theoplayer.android.api.source.ssai.CustomSsaiDescriptionSerializer
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.JsonTransformingSerializer
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 object UplynkSsaiDeserializer : CustomSsaiDescriptionSerializer {
     override fun fromJson(json: String): UplynkSsaiDescription {
-        return Json.decodeFromString(json)
+        return Json.decodeFromString(UplynkSsaiDescriptionKSerializer, json)
     }
 
     override fun toJson(value: CustomSsaiDescription): String {
-        return Json.encodeToString(value as UplynkSsaiDescription)
+        return Json.encodeToString(
+            UplynkSsaiDescriptionKSerializer,
+            value as UplynkSsaiDescription
+        )
     }
+}
+
+private object UplynkSsaiDescriptionKSerializer :
+    JsonTransformingSerializer<UplynkSsaiDescription>(UplynkSsaiDescription.serializer()) {
+
+    override fun transformSerialize(element: JsonElement): JsonElement = JsonObject(
+        // Add integration to JSON
+        buildMap {
+            put("integration", JsonPrimitive(UplynkConnector.INTEGRATION_ID))
+            putAll(element.jsonObject)
+        }
+    )
+
+    override fun transformDeserialize(element: JsonElement): JsonElement {
+        // Validate integration in JSON
+        val integration = element.jsonObject["integration"]?.jsonPrimitive
+        require(integration == JsonPrimitive(UplynkConnector.INTEGRATION_ID))
+        return element
+    }
+
 }
