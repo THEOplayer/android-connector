7019441d129f5acec707be2fc2de36b12bc7d96c
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-06-16 16:57:37 +0200

    Simplify SourceHelpers

--- connectors/yospace/src/main/java/com/theoplayer/android/connector/yospace/internal/SourceHelpers.kt
+++ connectors/yospace/src/main/java/com/theoplayer/android/connector/yospace/internal/SourceHelpers.kt
@@ -4,25 +4,9 @@ import com.theoplayer.android.api.source.SourceDescription
 import com.theoplayer.android.api.source.TypedSource
 
 internal fun SourceDescription.replaceSources(sources: List<TypedSource>): SourceDescription {
-    return SourceDescription.Builder(*sources.toTypedArray()).apply {
-        ads(*ads.toTypedArray())
-        textTracks(*textTracks.toTypedArray())
-        poster?.let { poster(it) }
-        metadata?.let { metadata(it) }
-        timeServer?.let { timeServer(it) }
-    }.build()
+    return copy(sources = sources)
 }
 
 internal fun TypedSource.replaceSrc(src: String): TypedSource {
-    return TypedSource.Builder(src).apply {
-        drm?.let { drm(it) }
-        type?.let { type(it) }
-        liveOffset?.let { liveOffset(it) }
-        ssai?.let { ssai(it) }
-        hlsDateRange?.let { hlsDateRange(it) }
-        timeServer?.let { timeServer(it) }
-        lowLatency?.let { lowLatency(it) }
-        hls?.let { hls(it) }
-        dash?.let { dash(it) }
-    }.build()
+    return copy(src = src)
 }
