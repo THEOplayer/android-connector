834f856230e63ccb3ce6c11f79d993f003d93422
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-09-08 12:50:36 +0200

    Always report Media3 as pipeline for Conviva

--- connectors/analytics/conviva/src/main/java/com/theoplayer/android/connector/analytics/conviva/ConvivaHandler.kt
+++ connectors/analytics/conviva/src/main/java/com/theoplayer/android/connector/analytics/conviva/ConvivaHandler.kt
@@ -18,7 +18,6 @@ import com.theoplayer.android.api.event.EventListener
 import com.theoplayer.android.api.event.ads.AdEvent
 import com.theoplayer.android.api.event.player.*
 import com.theoplayer.android.api.player.Player
-import com.theoplayer.android.api.source.PlaybackPipeline
 import com.theoplayer.android.api.source.SourceDescription
 import com.theoplayer.android.connector.analytics.conviva.ads.AdReporter
 import com.theoplayer.android.connector.analytics.conviva.utils.ErrorReportBuilder
@@ -171,13 +170,7 @@ class ConvivaHandler(
             }
             maybeReportPlaybackEnded()
             currentSource = player.source
-            customMetadata = mapOf("playbackPipeline" to
-                when(event.playbackPipeline) {
-                    PlaybackPipeline.LEGACY -> "legacy"
-                    PlaybackPipeline.MEDIA3 -> "media3"
-                    else -> "NA"
-                }
-            )
+            customMetadata = mapOf("playbackPipeline" to "media3")
         }
 
         onEnded = EventListener<EndedEvent> {
