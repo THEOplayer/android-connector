4284af9deae32fdd05e29c08f1974ed5e5390657
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-09-30 11:02:41 +0200

    Fix functional object listeners

--- connectors/analytics/conviva/src/main/java/com/theoplayer/android/connector/analytics/conviva/theolive/THEOliveReporter.kt
+++ connectors/analytics/conviva/src/main/java/com/theoplayer/android/connector/analytics/conviva/theolive/THEOliveReporter.kt
@@ -3,6 +3,7 @@ package com.theoplayer.android.connector.analytics.conviva.theolive
 import android.util.Log
 import com.conviva.sdk.ConvivaSdkConstants
 import com.conviva.sdk.ConvivaVideoAnalytics
+import com.theoplayer.android.api.event.EventListener
 import com.theoplayer.android.api.event.player.theolive.EndpointLoadedEvent
 import com.theoplayer.android.api.event.player.theolive.IntentToFallbackEvent
 import com.theoplayer.android.api.event.player.theolive.TheoLiveEventTypes
@@ -14,57 +15,50 @@ import com.theoplayer.android.connector.analytics.conviva.utils.flattenErrorObje
 private const val TAG = "THEOliveReporter"
 
 class THEOliveReporter(val player: Player, val convivaVideoAnalytics: ConvivaVideoAnalytics) {
+
+    private val onEndPointLoaded =
+        EventListener<EndpointLoadedEvent> { handleEndPointLoaded(it) }
+    private val onIntentToFallback =
+        EventListener<IntentToFallbackEvent> { handleIntentToFallback(it) }
+
     init {
         addEventListeners()
     }
 
-    private fun onEndPointLoaded(event: EndpointLoadedEvent) {
+    private fun handleEndPointLoaded(event: EndpointLoadedEvent) {
         val endpoint = event.getEndpoint()
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "onEndPointLoaded - endpoint: $endpoint")
         }
 
-        convivaVideoAnalytics.setContentInfo(
-            mutableMapOf<String, String>().apply {
-                endpoint.cdn?.let { cdn ->
-                    put(ConvivaSdkConstants.DEFAULT_RESOURCE, cdn)
-                }
-            } as ConvivaMetadata
-        )
+        convivaVideoAnalytics.setContentInfo(mutableMapOf<String, String>().apply {
+            endpoint.cdn?.let { cdn ->
+                put(ConvivaSdkConstants.DEFAULT_RESOURCE, cdn)
+            }
+        } as ConvivaMetadata)
     }
 
-    private fun onIntentToFallback(event: IntentToFallbackEvent) {
+    private fun handleIntentToFallback(event: IntentToFallbackEvent) {
         val reason = event.reason
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "IntentToFallbackEvent - reason: ${reason ?: "NA"}")
         }
         convivaVideoAnalytics.reportPlaybackEvent(
-            "intentToFallback",
-            mutableMapOf<String, Any>().apply {
+            "intentToFallback", mutableMapOf<String, Any>().apply {
                 reason?.let {
                     put("reason", flattenErrorObject(reason))
                 }
-            }
-        )
+            })
     }
 
     private fun addEventListeners() {
-        player.theoLive.addEventListener(TheoLiveEventTypes.ENDPOINTLOADED, this::onEndPointLoaded)
-        player.theoLive.addEventListener(
-            TheoLiveEventTypes.INTENTTOFALLBACK,
-            this::onIntentToFallback
-        )
+        player.theoLive.addEventListener(TheoLiveEventTypes.ENDPOINTLOADED, onEndPointLoaded)
+        player.theoLive.addEventListener(TheoLiveEventTypes.INTENTTOFALLBACK, onIntentToFallback)
     }
 
     private fun removeEventListeners() {
-        player.theoLive.removeEventListener(
-            TheoLiveEventTypes.ENDPOINTLOADED,
-            this::onEndPointLoaded
-        )
-        player.theoLive.removeEventListener(
-            TheoLiveEventTypes.INTENTTOFALLBACK,
-            this::onIntentToFallback
-        )
+        player.theoLive.removeEventListener(TheoLiveEventTypes.ENDPOINTLOADED, onEndPointLoaded)
+        player.theoLive.removeEventListener(TheoLiveEventTypes.INTENTTOFALLBACK, onIntentToFallback)
     }
 
     fun destroy() {
